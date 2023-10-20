2ccd1db2cd77d91b25b39a3eda20b0c5eda1220e
RinaWolfeak <117139822+RinaWolfeak@users.noreply.github.com>
2023-10-20 12:19:33 +0500

    Update start-batman-adv.sh

--- start-batman-adv.sh
+++ start-batman-adv.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+if ! grep -R "batman-adv" /etc/modules
+then
+  echo 'batman-adv' | tee --append /etc/modules
+fi
+if ! grep -R "denyinterfaces wlan0" /etc/dhcpcd.conf
+then
+  echo 'denyinterfaces wlan0' | tee --append /etc/dhcpcd.conf
+fi
+if [ ! -f /etc/network/interfaces.d/bat0 ]
+then
+  cp /source/bat0 /etc/network/interfaces.d/
+fi
+if [ ! -f /etc/network/interfaces.d/wlan0 ]
+then
+ cp /source/wlan0 /etc/network/interfaces.d/
+fi
+
 # batman-adv interface to use
 batctl if add wlan0
 ifconfig bat0 mtu 1468
 
 # Tell batman-adv this is a gateway client
-batctl gw_mode client
+#batctl gw_mode client
 
 # Activates batman-adv interfaces
 ifconfig wlan0 up
