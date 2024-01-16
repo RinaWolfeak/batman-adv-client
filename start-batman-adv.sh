bfc16d0a000af9fc9b684147c08a7cc64c1b700c
RinaWolfeak <117139822+RinaWolfeak@users.noreply.github.com>
2024-01-16 11:31:02 +0500

    Update start-batman-adv.sh

--- start-batman-adv.sh
+++ start-batman-adv.sh
@@ -23,7 +23,7 @@ batctl if add wlan0
 ifconfig bat0 mtu 1468
 
 # Tell batman-adv this is a gateway client
-batctl gw_mode client
+nsenter -t 1 -m -u -i -n batctl gw_mode client
 
 # Activates batman-adv interfaces
 ifconfig wlan0 up
